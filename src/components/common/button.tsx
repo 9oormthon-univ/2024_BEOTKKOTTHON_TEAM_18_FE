f4a9eb48926fe2500d7c9a6eaaeb99cddc846319
sscoderati <69716992+sscoderati@users.noreply.github.com>
2024-03-19 22:57:58 +0900

    ♻️ Refactor(#5): Button 공통 컴포넌트 리팩터링

--- src/components/common/button.tsx
+++ src/components/common/button.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
+import { cn } from '@/lib/utils.ts';
 
-interface ButtonProps {
-  onClick: () => void;
-  text: string;
-  disabled?: boolean;
+interface ButtonProps extends React.ComponentProps<'button'> {
+  variant: 'primary' | 'secondary';
+  size: 'sm' | 'md' | 'lg';
+  children: React.ReactNode;
 }
 
-const Button: React.FC<ButtonProps> = ({ onClick, text, disabled = false }) => {
+const Button = ({ variant, size, children, ...otherProps }: ButtonProps) => {
+  const buttonType = {
+    primary: 'bg-hc-blue text-hc-white',
+    secondary: 'bg-gray-500'
+  };
+  const buttonSize = {
+    sm: 'px-4 py-2 text-sm',
+    md: 'px-8 py-3 text-base',
+    lg: 'px-12 py-4 text-lg'
+  };
   return (
     <button
-      onClick={onClick}
-      disabled={disabled}
-      className="px-12 py-3 text-lg font-semibold text-white bg-blue-500 rounded-3xl focus:outline-none w-[338px] h-[54px]">
-      {text}
+      {...otherProps}
+      className={cn(
+        'px-12 py-3 text-lg font-semibold text-white bg-blue-500 rounded-3xl focus:outline-none w-[338px] h-[54px]',
+        buttonType[variant],
+        buttonSize[size]
+      )}>
+      {children}
     </button>
   );
 };
