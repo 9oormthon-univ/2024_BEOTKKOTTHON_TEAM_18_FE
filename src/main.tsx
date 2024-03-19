bec9018707c09171b91219431d1bf97b61853041
sscoderati <69716992+sscoderati@users.noreply.github.com>
2024-03-19 15:48:36 +0900

    ♻️ Refactor(#6): js -> ts 변환

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
-import router from './routes/router.jsx';
+import router from './routes/router.tsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <RouterProvider router={router} />
 );
