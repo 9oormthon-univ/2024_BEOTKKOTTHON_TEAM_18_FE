999b8d203c729d160dfe06abf1d33a2f6a2ba650
sscoderati <69716992+sscoderati@users.noreply.github.com>
2024-03-21 21:56:39 +0900

    📝 Chore(#12): selectOption 상수 추가
    - 추후 BE와 협의 필요

---
+++ src/constants/selectOptions.ts
@@ -0,0 +1,23 @@
+export const DdeepPurpose = [
+  { value: '맛집', label: '맛집' },
+  { value: '카페', label: '카페' },
+  { value: '영화', label: '영화' },
+  { value: '전시회', label: '전시회' },
+  { value: '보드게임', label: '보드게임' },
+  { value: '방탈출', label: '방탈출' },
+  { value: '코인노래방', label: '코인노래방' },
+  { value: '배달음식', label: '배달음식' }
+];
+
+export const DdeepRecruitingNumber = [
+  { value: 1, label: '1명' },
+  { value: 2, label: '2명' },
+  { value: 3, label: '3명' },
+  { value: 4, label: '4명' },
+  { value: 5, label: '5명' },
+  { value: 6, label: '6명' },
+  { value: 7, label: '7명' },
+  { value: 8, label: '8명' },
+  { value: 9, label: '9명' },
+  { value: 10, label: '10명' }
+];
