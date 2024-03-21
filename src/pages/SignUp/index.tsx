e7d3b4090ba2947c1a89b9af2038717e5cdf05fa
Seungyeon Han <tiffanyhansy@gmail.com>
2024-03-22 04:25:40 +0900

    ✨ Feat(#21): 회원가입 화면 구성

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,47 @@
+import Button from '@/components/common/Button';
+import Input from '@/components/common/Input';
+
 const SignUp = () => {
-    return <div>SignUp</div>;
-  };
-  
-  export default SignUp;
+  return (
+    <div>
+      <form>
+        <legend>아이디*</legend>
+        <div className="flex">
+          <Input variant="white" />
+          <Button
+            variant="primary"
+            size="sm"
+            children="중복확인"
+          />
+        </div>
+
+        <legend>닉네임*</legend>
+        <div className="flex">
+          <Input variant="white" />
+          <Button
+            variant="primary"
+            size="sm"
+            children="중복확인"
+          />
+        </div>
+
+        <legend>비밀번호*</legend>
+        <Input
+          variant="white"
+          placeholder="영어, 특수문자 포함 8자리 이상"
+        />
+
+        <legend>비밀번호 확인*</legend>
+        <Input variant="white" />
+      </form>
+
+      <Button
+        variant="primary"
+        size="lg"
+        children="회원가입"
+      />
+    </div>
+  );
+};
+
+export default SignUp;
