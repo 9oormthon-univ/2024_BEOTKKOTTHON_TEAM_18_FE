b744bcbd9e4f28edd9c7cd5038384816ebc19955
Seungyeon Han <tiffanyhansy@gmail.com>
2024-03-23 01:08:59 +0900

    ✨ Feat(#21): 비밀번호 유효성 검사, 비밀번호 재확인 구현

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,7 +1,37 @@
 import Button from '@/components/common/Button';
 import Input from '@/components/common/Input';
+import { useState } from 'react';
 
 const SignUp = () => {
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [isPasswordValid, setIsPasswordValid] = useState(true);
+
+  const validatePassword = () => {
+    const passwordRegex =
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return passwordRegex.test(password);
+  };
+
+  // 비밀번호 입력 시 호출되는 함수
+  const handlePasswordChange = (error) => {
+    setPassword(error.target.value);
+    setIsPasswordValid(validatePassword());
+  };
+
+  // 비밀번호 확인 입력 시 호출되는 함수
+  const handleConfirmPasswordChange = (error) => {
+    setConfirmPassword(error.target.value);
+  };
+
+  // 회원가입 버튼 클릭 시 호출되는 함수
+  const handleSignUp = () => {
+    if (password !== confirmPassword) {
+      alert('비밀번호가 일치하지 않습니다.');
+      return;
+    }
+  };
+
   return (
     <div className={'flex justify-center items-center h-screen'}>
       <div className="flex-row items-center justify-center h-screen">
@@ -46,7 +76,17 @@ const SignUp = () => {
             placeholder="영어, 특수문자 포함 8자리 이상"
             size="lg"
             type="password"
+            value={password}
+            onChange={handlePasswordChange}
           />
+          {!isPasswordValid && (
+            <div>
+              <hr className="w-5/6 mt-1 border ml-7 text-hc-coral" />
+              <p className="text-[12px] text-hc-coral">
+                영어, 특수문자 포함 8자리 이상이어야 합니다.
+              </p>
+            </div>
+          )}
 
           <legend className="mt-[9px] ml-[3px]  font-semibold text-hc-grayDark">
             비밀번호 확인 *
@@ -55,17 +95,26 @@ const SignUp = () => {
             variant="white"
             size="lg"
             type="password"
+            value={confirmPassword}
+            onChange={handleConfirmPasswordChange}
           />
+
+          {password !== confirmPassword && confirmPassword.length > 0 && (
+            <div>
+              <hr className="w-5/6 mt-1 border ml-7 text-hc-coral" />{' '}
+              <p className="text-red-500">비밀번호가 일치하지 않습니다.</p>
+            </div>
+          )}
         </form>
 
         <Button
           variant="primary"
           size="lg"
           children="회원가입"
+          onClick={handleSignUp}
         />
       </div>
     </div>
   );
 };
-
 export default SignUp;
