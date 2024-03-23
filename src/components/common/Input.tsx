3a2cf54b62e16d8697e42fa89bcd63a2d869ba1b
Seungyeon Han <tiffanyhansy@gmail.com>
2024-03-23 15:54:08 +0900

    📝 Chore(#21): Input 컴포넌트 구조 수정

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,34 +1,33 @@
 import React, { ComponentPropsWithoutRef, ForwardedRef } from 'react';
 import { cn } from '@/lib/utils.ts';
 
-interface InputProps extends Omit<ComponentPropsWithoutRef<'input'>, 'size'> {
+interface InputProps extends ComponentPropsWithoutRef<'input'> {
   variant: 'gray' | 'white';
-  size: 'sm' | 'lg';
 }
 
-const Input = ({ variant, size, ...otherProps }: InputProps) => {
-  const inputVariant = {
-    gray: 'bg-hc-grayLight',
-    white: 'bg-hc-white'
-  };
+const Input = React.forwardRef(
+  (
+    { variant, ...otherProps }: InputProps,
+    ref: ForwardedRef<HTMLInputElement>
+  ) => {
+    const inputVariant = {
+      gray: 'bg-hc-grayLight',
+      white: 'bg-hc-white'
+    };
 
-  const inputSize = {
-    lg: 'w-[338px]',
-    sm: 'w-[238px]'
-  };
+    return (
+      <input
+        className={cn(
+          'w-[338px] h-[54px] rounded-3xl border-2 border-hc-grayLight focus:outline-none text-sm font-semibold px-6 py-2.5 mt-5',
 
-  return (
-    <input
-      className={cn(
-        'h-[54px] rounded-3xl border-2 border-hc-grayLight focus:outline-none text-sm font-semibold px-6 py-2.5 mt-[5px]',
-
-        inputVariant[variant],
-        inputSize[size]
-      )}
-      {...otherProps}
-    />
-  );
-};
+          inputVariant[variant]
+        )}
+        {...otherProps}
+        ref={ref}
+      />
+    );
+  }
+);
 
 Input.displayName = 'Input';
 
