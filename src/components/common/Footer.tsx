19f60d0c101f5671a00b5028788d42a4049e3905
sscoderati <69716992+sscoderati@users.noreply.github.com>
2024-03-23 19:57:07 +0900

    ✨ Feat(#11): 알림 아이콘 스타일 일부 변경 및 안읽은 알림 표시 기능 추가

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,11 +1,11 @@
 import FooterElement from '@/components/common/FooterElement.tsx';
 import HomeIcon from '@/components/icons/HomeIcon.tsx';
-import MessageIcon from '@/components/icons/MessageIcon.tsx';
 import CirclePlusIcon from '@/components/icons/CirclePlusIcon.tsx';
 import UserIcon from '@/components/icons/UserIcon.tsx';
 import { useLocation } from 'react-router-dom';
 import { footerLabelPathInfo } from '@/constants/footerLabelPathInfo.ts';
 import { blueDarkHex, grayDarkHex } from '@/constants';
+import NotificationIcon from '@/components/icons/NotificationIcon.tsx';
 
 const Footer = () => {
   const location = useLocation();
@@ -30,12 +30,13 @@ const Footer = () => {
       />
       <FooterElement
         icon={
-          <MessageIcon
-            stroke={
+          <NotificationIcon
+            fill={
               location.pathname === footerLabelPathInfo['notification'].path
                 ? blueDarkHex
                 : grayDarkHex
             }
+            hasNew={true}
           />
         }
         label={'알림'}
