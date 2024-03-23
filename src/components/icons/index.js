1a4719ca3a5c086634b02d3518881da0acf10da2
sscoderati <69716992+sscoderati@users.noreply.github.com>
2024-03-23 19:49:00 +0900

    ✨ Feat(#11): 메세지 아이콘 컴포넌트 알림 아이콘 컴포넌트로 교체

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,7 +1,7 @@
 export { default as CirclePlusIcon } from './CirclePlusIcon';
-export { default as CommentButtonIcon } from './CommentButtonIcon.js';
+export { default as CommentButtonIcon } from './CommentButtonIcon';
 export { default as HcLogoLabel } from './HcLogoLabel';
 export { default as HcLogoPic } from './HcLogoPic';
 export { default as HomeIcon } from './HomeIcon';
-export { default as MessageIcon } from './MessageIcon';
+export { default as NotificationIcon } from './NotificationIcon.js';
 export { default as UserIcon } from './UserIcon';
