67f56a81e7ec56d27a89a43fd9786fc9afa85458
Seungyeon Han <tiffanyhansy@gmail.com>
2024-03-23 19:26:29 +0900

    💄 Style(#21): 회원가입 페이지 스타일 수정

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,8 +1,7 @@
-import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Form, FormItem, FormMessage } from '@/components/ui/form.tsx';
+import { Form, FormItem } from '@/components/ui/form.tsx';
 import Button from '@/components/common/Button';
 import Input from '@/components/common/Input';
 import { isSingupValidSchema, Sign } from '@/schemas/signupSchema';
@@ -24,54 +23,72 @@ const SignUp = () => {
         <form onSubmit={handleSubmit}>
           {/* 아이디 */}
           <FormItem className={'flex flex-col mb-4'}>
-            <p className={'text-lg font-semibold'}>아이디 *</p>
-            <Input
-              variant={'white'}
-              {...form.register('loginId')}
-            />
-            {form.formState.errors.loginId && (
-              <div className={'text-hc-coral text-[12px] mt-1'}>
-                {form.formState.errors.loginId.message}
+            <p className={'text-lg font-semibold text-hc-grayDark'}>아이디 *</p>
+            <div className="flex justify-between">
+              <div>
+                <Input
+                  variant={'white'}
+                  {...form.register('loginId')}
+                  className={'w-[238px]'}
+                />
+                {form.formState.errors.loginId && (
+                  <div className={'text-hc-coral text-[12px] mt-1 ml-2'}>
+                    {form.formState.errors.loginId.message}
+                  </div>
+                )}
               </div>
-            )}
+              <Button
+                variant={'primary'}
+                size={'sm'}
+                type={'submit'}
+                className={'mt-[20px]'}>
+                중복확인
+              </Button>
+            </div>
           </FormItem>
-          <Button
-            variant={'primary'}
-            size={'sm'}
-            type={'submit'}>
-            중복확인
-          </Button>
 
           {/* 닉네임 */}
           <FormItem className={'flex flex-col mb-4'}>
-            <p className={'text-lg font-semibold'}>닉네임 *</p>
-            <Input
-              variant={'white'}
-              {...form.register('nickname')}
-            />
-            {form.formState.errors.nickname && (
-              <div className={'text-hc-coral text-[12px] mt-1'}>
-                {form.formState.errors.nickname.message}
+            <p className={'text-lg font-semibold  text-hc-grayDark'}>
+              닉네임 *
+            </p>
+            <div className="flex justify-between">
+              <div>
+                <Input
+                  variant={'white'}
+                  {...form.register('nickname')}
+                  className={'w-[238px]'}
+                />
+                {form.formState.errors.nickname && (
+                  <div className={'text-hc-coral text-[12px] mt-1 ml-2'}>
+                    {form.formState.errors.nickname.message}
+                  </div>
+                )}
               </div>
-            )}
+
+              <Button
+                variant={'primary'}
+                size={'sm'}
+                type={'submit'}
+                className={'mt-[20px]'}>
+                중복확인
+              </Button>
+            </div>
           </FormItem>
-          <Button
-            variant={'primary'}
-            size={'sm'}
-            type={'submit'}>
-            중복확인
-          </Button>
 
           {/* 비밀번호 */}
           <FormItem className={'flex flex-col mb-4'}>
-            <p className={'text-lg font-semibold'}>비밀번호 *</p>
+            <p className={'text-lg font-semibold  text-hc-grayDark'}>
+              비밀번호 *
+            </p>
             <Input
               variant={'white'}
               {...form.register('password')}
               type="password"
+              placeholder="영어, 특수문자 포함 8자리 이상 "
             />
             {form.formState.errors.password && (
-              <div className={'text-hc-coral text-[12px] mt-1'}>
+              <div className={'text-hc-coral text-[12px] mt-1 ml-2'}>
                 {form.formState.errors.password.message}
               </div>
             )}
@@ -79,14 +96,16 @@ const SignUp = () => {
 
           {/* 비밀번호 확인  */}
           <FormItem className={'flex flex-col mb-4'}>
-            <p className={'text-lg font-semibold'}>비밀번호 확인 *</p>
+            <p className={'text-lg font-semibold  text-hc-grayDark'}>
+              비밀번호 확인 *
+            </p>
             <Input
               variant={'white'}
               {...form.register('checkPassword')}
               type="password"
             />
             {form.formState.errors.checkPassword && (
-              <div className={'text-hc-coral text-[12px] mt-1'}>
+              <div className={'text-hc-coral text-[12px] mt-1 ml-2'}>
                 {form.formState.errors.checkPassword.message}
               </div>
             )}
